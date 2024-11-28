368a3de4e87bc8369d68f5e4821a89e0cce56f68
HackerMasterEango <mastereango@gmail.com>
2024-11-27 18:40:05 -0800

    v1 tier list component done

--- src/app/(anime-mobile-games)/girls-frontline-exilium/tier-list/page.tsx
+++ src/app/(anime-mobile-games)/girls-frontline-exilium/tier-list/page.tsx
@@ -1,11 +1,69 @@
 import TierList from '@/components/TierList/TierList'
 import { ScrollArea } from '@/components/ui/scroll-area'
 
+type HeavenBurnRedCategories = 'Blaster' | 'Tank' | 'Support' | 'Healer'
+type HeavenBurnRedTiers = 'S' | 'A' | 'B' | 'C' | 'D' | 'E'
+
 function Page() {
   return (
     <ScrollArea className="h-screen w-full">
       <div className="container px-4 py-24 mx-auto ">
-        <TierList />
+        <TierList<HeavenBurnRedCategories, HeavenBurnRedTiers>
+          categories={[
+            {
+              name: 'Blaster',
+              colorClass: 'bg-red-500'
+            },
+            {
+              name: 'Tank',
+              colorClass: 'bg-blue-500'
+            },
+            {
+              name: 'Support',
+              colorClass: 'bg-green-500'
+            },
+            {
+              name: 'Healer',
+              colorClass: 'bg-yellow-300'
+            }
+          ]}
+          tiers={[
+            {
+              tierName: 'B',
+              colorClass: 'bg-blue-500',
+              placements: {
+                Blaster: [
+                  {
+                    id: '1',
+                    imgUrl: 'Character 1'
+                  },
+                  {
+                    id: '2',
+                    imgUrl: 'Character 1'
+                  }
+                ],
+                Tank: [
+                  {
+                    id: '1',
+                    imgUrl: 'Character 1'
+                  }
+                ],
+                Support: [
+                  {
+                    id: '1',
+                    imgUrl: 'Character 1'
+                  }
+                ],
+                Healer: [
+                  {
+                    id: '1',
+                    imgUrl: 'Character 1'
+                  }
+                ]
+              }
+            }
+          ]}
+        />
       </div>
     </ScrollArea>
   )
--- src/components/TierList/TierList.tsx
+++ src/components/TierList/TierList.tsx
@@ -1,12 +1,40 @@
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import React from 'react'
 
-const TierList = ({ numCols = 2 }) => {
+type TierListCategory<T extends string> = {
+  name: T
+  colorClass: string
+}
+
+// actual character image placed on tier list.
+type TieredCharacter = {
+  id: string
+  imgUrl: string
+}
+
+// lookup type of character type to tiered character
+// e.g. in heavens burns red: Blaster -> TieredCharacter[]
+type Placements<C extends string> = {
+  [K in C]: TieredCharacter[]
+}
+
+type Tier<C extends string, T extends string> = {
+  tierName: T
+  colorClass: string
+  placements: Placements<C>
+}
+
+type TierListProps<CategoryType extends string, TierNameType extends string> = {
+  categories: TierListCategory<CategoryType>[]
+  tiers: Tier<CategoryType, TierNameType>[]
+}
+const TierList = <C extends string, T extends string>({ categories, tiers }: TierListProps<C, T>) => {
+  const numCols = categories.length
+
   return (
     <div className="w-full">
-      {/* TODO:      */}
       <div
-        // copy above into inline style
         className="grid  gap-2 "
         style={{
           gridTemplateColumns: `auto ${' 1fr'.repeat(numCols)}`
@@ -21,51 +49,26 @@ const TierList = ({ numCols = 2 }) => {
             }}
           >
             <div className="w-12" />
-            <Columns />
+            <Columns<C> categories={categories} numCols={numCols} />
           </div>
         </div>
-        {/* Rows */}
-        <Row />
-        <Row /> <Row />
-        <Row />
-        <Row />
-        <Row /> <Row />
-        <Row />
-        <Row />
-        <Row /> <Row />
-        <Row />
-        <Row />
-        <Row /> <Row />
-        <Row />
-        <Row />
-        <Row /> <Row />
-        <Row />
-        <Row />
-        <Row /> <Row />
-        <Row />
-        <Row />
-        <Row /> <Row />
-        <Row />
-        <Row />
-        <Row /> <Row />
-        <Row />
-        <Row />
-        <Row /> <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        {/* ... more rows */}
+        {tiers.map(tier => (
+          <Row<C, T> key={tier.tierName} tier={tier} categories={categories} />
+        ))}
       </div>
     </div>
   )
 }
 
-const Columns = ({ numCols = 2 }) => {
+type ColumnsProps<C extends string> = {
+  categories: TierListCategory<C>[]
+  numCols: number
+}
+const Columns = <C extends string>({ categories, numCols }: ColumnsProps<C>) => {
   return (
     <div
-      className={` bg-dark-700 p-4 rounded-lg`}
+      // -mx-2 px-2 ...... nice hacking!
+      className="bg-dark-700 py-4 -mx-2 px-2 rounded-lg"
       style={{
         gridColumn: `span ${numCols} / span ${numCols}`
       }}
@@ -76,30 +79,34 @@ const Columns = ({ numCols = 2 }) => {
           gridTemplateColumns: `repeat(${numCols}, minmax(0, 1fr))`
         }}
       >
-        <div className="bg-red-500 rounded-full text-center">DPS</div>
-        <div className="bg-blue-500 rounded-full text-center">Healer</div>
+        {categories.map(category => (
+          <div key={category.name} className={cn('rounded-full text-center', category.colorClass)}>
+            {category.name}
+          </div>
+        ))}
       </div>
     </div>
   )
 }
 
-//
-const Row = () => {
+type RowProps<C extends string, T extends string> = {
+  tier: Tier<C, T>
+  categories: TierListCategory<C>[]
+}
+const Row = <C extends string, T extends string>({ tier, categories }: RowProps<C, T>) => {
   return (
     <>
-      <div className="bg-blue-500 w-12  flex items-center justify-center h-full">S</div>
-      <div className="flex gap-2 flex-wrap bg-dark-800 rounded-lg items-center p-4">
-        <CharacterCard />
-        <CharacterCard />
-      </div>
-      <div className="flex gap-2 flex-wrap bg-dark-800 rounded-lg items-center p-4">
-        <CharacterCard />
-        <CharacterCard />
-      </div>
+      <div className={cn('w-12 flex items-center justify-center h-full', tier.colorClass)}>{tier.tierName}</div>
+      {categories.map(category => (
+        <div key={category.name} className="flex gap-2 flex-wrap bg-dark-800 rounded-lg items-center p-4">
+          {tier.placements[category.name].map(character => (
+            <CharacterCard key={character.id} />
+          ))}
+        </div>
+      ))}
     </>
   )
 }
-
 export function CharacterCard() {
   return (
     <div className="w-[100px] h-[100px] overflow-hidden rounded-lg cursor-pointer">

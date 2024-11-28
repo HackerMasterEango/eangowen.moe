3ce927f8555bc2d6e6350db49b9e866c38fad949
MasterHackerEango <mastereango@gmail.com>
2024-11-28 16:14:59 -0700

    allow user sign in with a username

--- src/actions/loginUsers.ts
+++ src/actions/loginUsers.ts
@@ -1,94 +1,81 @@
 'use server'
 import { supabaseServerClient } from '@/lib/supabase/serverClient'
+import { SupabaseClient } from '@supabase/supabase-js'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
-const resolveEmailFromIdentifier = async (identifier: string) => {
-  //const supabase = await supabaseServerClient()
+type ResolveEmailIdentifierResponse = {
+  email: string | null
+  error: boolean
+}
+const resolveEmailFromIdentifier = async (supabase: SupabaseClient,identifier: string):
+Promise<ResolveEmailIdentifierResponse> => {
+
   // Default to treating the identifier as an email
   if (identifier.includes('@')) {
-    return { email: identifier, error: null }
+    return { email: identifier, error: false }
   }
-  /* TODO: Implement solution from this:
-  https://stackoverflow.com/questions/78550922/how-do-i-authorise-users-with-username-in-supabase
+
   // Treat the identifier as a username and fetch the associated email
-  const { data: profileData, error: profileError } = await supabase
+  const { data, error } = await supabase
     .from('profiles')
-    .select('id')
+    .select('email')
     .eq('username', identifier)
-    .single();
+    .single()
 
-  if (profileError || !profileData) {
-    return { email: null, error: profileError || new Error('Username not found') };
+  if (error || !data) {
+    return { email: null, error: true }
   }
 
-  return { email: profileData.email, error: null };*/
-
-  return { email: identifier, error: null }
+  return { email: data.email, error: false }
 }
-
 export const loginUser = async (_: unknown, formData: FormData) => {
   const supabase = await supabaseServerClient()
 
-  const userIdentifier = formData.get('userIdentifier') as string //should change form name
+  // TODO: add zod validation
+  const userIdentifier = formData.get('userIdentifier') as string
   const password = formData.get('password') as string
-
-  let success = true
-
-  const email = (await resolveEmailFromIdentifier(userIdentifier)).email as string
-
-  const { error: authError } = await supabase.auth.signInWithPassword({
-    email,
-    password
-  })
-
-  if (authError) {
-    success = false
+  
+  const { email, error: emailError } = await resolveEmailFromIdentifier(supabase, userIdentifier)
+  if (emailError || !email) {
+    return { success: false }
   }
 
+  const { error: authError } = await supabase.auth.signInWithPassword({ email, password })
   revalidatePath('/', 'layout')
-
-  return { success, fieldData: { email, password } }
+  
+  return {
+    success: !authError,
+    ...((!authError && { fieldData: { email, password } }) || {})
+  }
 }
 
-//Expects email, username, and password in FormData
+// Expects email, username, and password in FormData
 export const signUp = async (formData: FormData) => {
   const supabase = await supabaseServerClient()
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
+  // TODO: use zod to validate form data
   const { error: authError } = await supabase.auth.signUp({
     email: formData.get('email') as string,
     password: formData.get('password') as string,
+    options: {
+      // NOTE: everything here will be synced in a public profiles table
+      data: {
+        username: formData.get('username') as string,
+        email: formData.get('email') as string
+      }
+    }
   })
 
   if (authError) {
-    console.log("Auth Error: ", authError)
     redirect('/error')
   }
 
-  /* username is now updated by trigger
-  //Might want to make this an atomic query
-  const { error: profileError } = await supabase
-        .from('profiles')
-        .insert([
-          {
-            id: authData!.user!.id, // Extract the user ID from auth table update. Enforce non-null
-            username: formData.get('username') as string //Nullable defined in schema
-          }
-        ])
-
-if (profileError) {
-  console.log("Profile Error: ", profileError)
-  redirect('/error')
-}
-*/
   revalidatePath('/', 'layout')
   redirect('/')
 }
 
 export const logOut = async () => {
-  console.log('logging out')
   const supabase = await supabaseServerClient()
 
   const { error } = await supabase.auth.signOut()
